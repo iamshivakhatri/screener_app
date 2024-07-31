1496ae57b58cdff980af93ae32ab05fef8efbd29
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-31 11:48:59 -0400

    Added script to get most active stocks.

---
+++ most_active.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define the URL and parameters for the API request
+url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/screener"
+querystring = {"list": "most_actives"}
+
+headers = {
+    "x-rapidapi-key": os.getenv("RAPID_API_KEY"),
+    "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
+}
+
+# Make the API request
+response = requests.get(url, headers=headers, params=querystring)
+
+print(json.dumps(response.json(), indent=2))
+
+# Save the response data to a file
+with open("most_actives.json", "w") as f:
+    f.write(json.dumps(response.json(), indent=2))
+
+# Load the response data from the file
+# with open("most_actives.json", "r") as f:
+#     data = json.load(f)
+
+# # Convert to DataFrame
+# df_most_actives = pd.DataFrame(data["body"])
+
+# # Select only the columns related to price
+# price_related_columns = ['symbol', 'regularMarketPrice', 'regularMarketPreviousClose',
+#                          'regularMarketDayHigh', 'regularMarketDayLow', 'regularMarketChangePercent']
+# df_price_data = df_most_actives[price_related_columns]
+# print(df_price_data)
--- trending_tickers.py
+++ trending_tickers.py
@@ -5,37 +5,30 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
-print(os.getenv("RAPID_API_KEY"))
+# # Define the URL and parameters for the API request
+# url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/screener"
+# querystring = {"list": "trending"}
 
-# Define the URL and parameters for the API request
-url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/screener"
-querystring = {"list": "trending"}
+# headers = {
+#     "x-rapidapi-key": os.getenv("RAPID_API_KEY"),
+#     "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
+# }
 
-headers = {
-    "x-rapidapi-key": os.getenv("RAPID_API_KEY"),
-    "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
-}
+# # Make the API request
+# response = requests.get(url, headers=headers, params=querystring)
 
-# Make the API request
-response = requests.get(url, headers=headers, params=querystring)
-
-print(json.dumps(response.json(), indent=2))
+# print(json.dumps(response.json(), indent=2))
 
 # # Save the response data to a file
 # with open("trending_tickers.json", "w") as f:
 #     f.write(json.dumps(response.json(), indent=2))
 
-# # Load the response data from the file
-# with open("trending_tickers.json", "r") as f:
-#     data = json.load(f)
+# Load the response data from the file
+with open("trending_tickers.json", "r") as f:
+    data = json.load(f)
 
-# # Convert to DataFrame
-# df_trending_tickers = pd.DataFrame(data["body"])
+ticker_list = data["body"]
 
-# # Select only the columns related to price
-# price_related_columns = ['symbol', 'regularMarketPrice', 'regularMarketPreviousClose',
-#                          'regularMarketDayHigh', 'regularMarketDayLow', 'regularMarketChangePercent']
-# df_price_data = df_trending_tickers[price_related_columns]
-# print(df_price_data)
+print(ticker_list)
