4ecbf2639d010b3abe0b79ec9604116a3435ea16
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-08-07 22:15:39 -0400

    Got the news and integrated with the app

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY')
 
 finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 
+news_cache = {}
 
 app = Flask(__name__)
 
@@ -33,6 +34,12 @@ def load_data():
         'undervalued_large_caps': df_undervalued_large_caps,
         'small_cap_gainers': df_small_cap_gainers
     }
+
+@app.route('/news/<ticker>')
+def get_news_for_ticker(ticker):
+    global news_cache
+    news = news_cache.get(ticker, [])
+    return news
 
 def get_news(ticker_list):
     news = {}
@@ -75,7 +82,9 @@ def index():
     active_ticker_list = active_filtered['symbol'].tolist()
     print(active_ticker_list)
     ticker_list = combine_lists(gainer_tickers_list, active_ticker_list)
-    news = get_news(ticker_list)
+    global news_cache
+    news_cache = get_news(ticker_list)
+    print("This is news", news_cache)
 
     return render_template('index.html', gainers=gainers_filtered.to_dict(orient='records'), active=active_filtered.to_dict(orient='records'))
 
@@ -107,6 +116,7 @@ def undervalued_large_caps():
 @app.route('/try_page')
 def try_page():
     return render_template('try.html')
+
 
 
 if __name__ == '__main__':
